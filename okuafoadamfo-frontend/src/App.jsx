bce3ff5a4a4f9cdf1dbde95eb8c3e225f511bb25
Dan Sid <dansidsaya@gmail.com>
2025-05-29 17:11:16 +0000

    Fix PredictDisease

--- okuafoadamfo-frontend/src/App.jsx
+++ okuafoadamfo-frontend/src/App.jsx
@@ -14,6 +14,7 @@ import About from "./pages/About";
 // Change to this (without .jsx extension):
 import PredictDisease from "./pages/PredictDisease";
 
+
 const App = () => {
   // Add the useEffect hook here
   useEffect(() => {
