b6a3f6f7edd73cba0fd84d6090ae09640eca1f48
Dan Sid <dansidsaya@gmail.com>
2025-05-29 15:26:29 +0000

    UpdatesofNetlify

--- okuafoadamfo-frontend/vite.config.js
+++ okuafoadamfo-frontend/vite.config.js
@@ -1,22 +1,35 @@
-// Connecting frontend to backend API
+// // Connecting frontend to backend API
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react(),tailwindcss()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         // We'd change the api link of the backend here
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx', '.json'] 
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/',
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        // We'd change the api link of the backend here
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'] 
+  plugins: [react()],
+  base: './', // This ensures relative paths for assets
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
